7cbedb07755f554d90d1cfa98a4cc4c6be54cc32
Germano Bruscato Corrêa <germanobruscato@gmail.com>
2026-08-27 23:59:20 -0300

    liquid glass-like inteface

--- set_bg.sh
+++ set_bg.sh
@@ -1,1 +1,7 @@
-swaybg -i '/home/gege/dotfiles/current_bg.jpg' -o '*' -m fill
+#!/bin/bash
+# swaybg -i '/home/gege/dotfiles/current_bg.jpg' -o '*' -m fill
+for (( ; ; )); do
+    for file in wallpapers/*.*; do
+        timeout 120 swaybg -i $file -o '*' -m fill
+    done
+done
