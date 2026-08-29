a96da0e356f7b4a4fd6789d9e8a011c702ba1d41
Germano Bruscato Corrêa <germanobruscato@gmail.com>
2026-08-29 18:14:34 -0300

    replaced swaybg for awww

--- set_bg.sh
+++ set_bg.sh
@@ -1,7 +1,30 @@
-#!/bin/bash
-# swaybg -i '/home/gege/dotfiles/current_bg.jpg' -o '*' -m fill
-for (( ; ; )); do
-    for file in wallpapers/*.*; do
-        timeout 120 swaybg -i $file -o '*' -m fill
-    done
+#!/bin/sh
+# Changes the wallpaper to a randomly chosen image in a given directory
+# at a set interval. Code from https://codeberg.org/LGFae/awww/src/branch/main/example_scripts/awww_randomize.sh
+
+DEFAULT_INTERVAL=300 # In seconds
+
+# if [ $# -lt 1 ] || [ ! -d "$1" ]; then
+#     printf "Usage:\n\t\e[1m%s\e[0m \e[4mDIRECTORY\e[0m [\e[4mINTERVAL\e[0m]\n" "$0"
+#     printf "\tChanges the wallpaper to a randomly chosen image in DIRECTORY every\n\tINTERVAL seconds (or every %d seconds if unspecified)." "$DEFAULT_INTERVAL"
+#     exit 1
+# fi
+
+awww-daemon &
+
+# See awww-img(1)
+RESIZE_TYPE="crop"
+export AWWW_TRANSITION_FPS="${AWWW_TRANSITION_FPS:-60}"
+export AWWW_TRANSITION_STEP="${AWWW_TRANSITION_STEP:-2}"
+
+while true; do
+    find "$1" -type f |
+        while read -r img; do
+            echo "$(</dev/urandom tr -dc a-zA-Z0-9 | head -c 8):$img"
+        done |
+        sort -n | cut -d':' -f2- |
+        while read -r img; do
+            awww img --resize="$RESIZE_TYPE" "$img"
+            sleep "${2:-$DEFAULT_INTERVAL}"
+        done
 done
